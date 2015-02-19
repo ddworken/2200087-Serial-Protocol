1580bc3982e33a9311e6448bafc503877401981d
David Dworken <david@daviddworken.com>
2015-02-19 11:00:34 -0500

    Clarified variable names

--- serialDecoder.py
+++ serialDecoder.py
@@ -161,69 +161,69 @@ def is0(digitDict):
 
 def strToFlags(strOfBytes): #Checks all possible flags that might be needed and returns a list containing all currently active flags
     flags = []
-    binArr = getArrFromStr(strOfBytes)
-    for index,binStr in enumerate(binArr):
-        binArr[index] = binStr[::-1]
-    if binArr[0][2] == '1':
+    binArray = getArrFromStr(strOfBytes)
+    for index,binStr in enumerate(binArray):
+        binArray[index] = binStr[::-1]
+    if binArray[0][2] == '1':
         flags.append('AC')
-    #if binArr[0][1] == '1': ###Don't display this because it will always be on since whenever we are getting input, it will be on.
+    #if binArray[0][1] == '1': ###Don't display this because it will always be on since whenever we are getting input, it will be on.
     #   flags.append('SEND')
-    if binArr[0][0] == '1':
+    if binArray[0][0] == '1':
         flags.append('AUTO')
-    if binArr[1][3] == '1':
+    if binArray[1][3] == '1':
         flags.append('CONTINUITY')
-    if binArr[1][2] == '1':
+    if binArray[1][2] == '1':
         flags.append('DIODE')
-    if binArr[1][1] == '1':
+    if binArray[1][1] == '1':
         flags.append('LOW BATTERY')
-    if binArr[1][0] == '1':
+    if binArray[1][0] == '1':
         flags.append('HOLD')
-    if binArr[10][0] == '1':
+    if binArray[10][0] == '1':
         flags.append('MIN')
-    if binArr[10][1] == '1':
+    if binArray[10][1] == '1':
         flags.append('REL DELTA')
-    if binArr[10][2] == '1':
+    if binArray[10][2] == '1':
         flags.append('HFE')
-    if binArr[10][3] == '1':
+    if binArray[10][3] == '1':
         flags.append('Percent')
-    if binArr[11][0] == '1':
+    if binArray[11][0] == '1':
         flags.append('SECONDS')
-    if binArr[11][1] == '1':
+    if binArray[11][1] == '1':
         flags.append('dBm')
-    if binArr[11][2] == '1':
+    if binArray[11][2] == '1':
         flags.append('n (1e-9)')
-    if binArr[11][3] == '1':
+    if binArray[11][3] == '1':
         flags.append('u (1e-6)')
-    if binArr[12][0] == '1':
+    if binArray[12][0] == '1':
         flags.append('m (1e-3)')
-    if binArr[12][1] == '1':
+    if binArray[12][1] == '1':
         flags.append('VOLTS')
-    if binArr[12][2] == '1':
+    if binArray[12][2] == '1':
         flags.append('AMPS')
-    if binArr[12][3] == '1':
+    if binArray[12][3] == '1':
         flags.append('FARADS')
-    if binArr[13][0] == '1':
+    if binArray[13][0] == '1':
         flags.append('M (1e6)')
-    if binArr[13][1] == '1':
+    if binArray[13][1] == '1':
         flags.append('K (1e3)')
-    if binArr[13][2] == '1':
+    if binArray[13][2] == '1':
         flags.append('OHMS')
-    if binArr[13][3] == '1':
+    if binArray[13][3] == '1':
         flags.append('Hz')
     return flags
 
 def strToDigits(strOfBytes): #converts a string of space separated hexadecimal bytes into numbers following the protocol in readme.md
-    binArr = getArrFromStr(strOfBytes) #Create an array of the binary values from those hexadecimal bytes
+    binArray = getArrFromStr(strOfBytes) #Create an array of the binary values from those hexadecimal bytes
     digits = ""
     for number in reversed(range(1,5)): #reversed rabge so that we iterate through values 4,3,2,1 in that order due to how serial protocol works (see readme.md)
-        out = processDigit(number,binArr)
+        out = processDigit(number,binArray)
         if out[1] == -1:
             print("Protocol Error: Please start an issue here: https://github.com/ddworken/2200087-Serial-Protocol/issues and include the following data: '" + strOfBytes + "'")
             exit(1)
         if out[0] == True: #append the decimal point if the decimalPointBool in the tuple is true
             digits += "."
         digits += str(out[1])
-    minusBool = bool(int(binArr[0][::-1][3])) #following the serial protocol, calculate whether or not a negative sign is needed
+    minusBool = bool(int(binArray[0][::-1][3])) #following the serial protocol, calculate whether or not a negative sign is needed
     if minusBool:
         digits = '-' + digits
     return digits
