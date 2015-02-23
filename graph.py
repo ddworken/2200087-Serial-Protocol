f4e36b99eefeabf9b8acd9b97eb6d91e6c9d843f
David Dworken <david@daviddworken.com>
2015-02-23 11:34:28 -0500

    Added getValues() method

--- graph.py
+++ graph.py
@@ -7,24 +7,21 @@ class grapher(object):
     subprocess = __import__('subprocess')
     graphOutput = []
     x = []
-    y = []
+    y = []    
 
     def __init__(self, x, y):
-        #self.gnuplot = subprocess.Popen(["/usr/bin/gnuplot"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        #gnuplot.stdin.write("set term dumb 79 25\n")
-        #gnuplot.stdin.write("plot '-' using 1:2 title 'Line1' with linespoints \n")
-        self.updateGraph(x,y)
+        self.update(x,y)
         self.graphOutput = self.getGraph()
         self.x = x
         self.y = y
-#        print self.graphOutput
 
-    def updateGraph(self, x, y):
+
+    def update(self, x, y):
         self.gnuplot = subprocess.Popen(["/usr/bin/gnuplot"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         self.gnuplot.stdin.write("set term dumb 79 25\n")
         self.gnuplot.stdin.write("plot '-' using 1:2 title 'Line1' with linespoints \n")
         for i,j in zip(x,y):
-            self.gnuplot.stdin.write("%f %f\n" % (i,j))
+            self.gnuplot.stdin.write("%f %f\n" % (i,j))    
         self.gnuplot.stdin.write("e\n")
         self.gnuplot.stdin.flush()
         i = 0
@@ -39,11 +36,13 @@ class grapher(object):
     def getGraph(self):
         return self.graphOutput
 
-    def graphAppend(self, xVal, yVal):
-        #self.x.append(xVal)
+    def getValues(self):
+        return zip(self.x,self.y)
+
+    def append(self, xVal, yVal):
         self.x = np.append(self.x, xVal)
         self.y = np.append(self.y, yVal)
-        self.updateGraph(self.x, self.y)
+        self.update(self.x, self.y)        
 
 
 x=np.linspace(0,2*np.pi,10)
@@ -52,7 +51,9 @@ grapher = grapher(x,y)
 graph = grapher.getGraph()
 for line in graph:
     print line
-grapher.graphAppend(1,2)
+grapher.append(1,2)
 graph = grapher.getGraph()
 for line in graph:
     print line
+print grapher.getValues()
+root@ddworken:~/dmm2# 
